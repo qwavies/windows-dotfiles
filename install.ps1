bf2e1ac940c33e878a140663740a5e7c614d9817
dtchen <dtchen247@gmail.com>
2025-07-05 13:28:54 +1000

    Added empty files and structure

--- install.ps1
+++ install.ps1
@@ -1,1 +1,21 @@
-echo "Hello";
+# install chocolatey(?)
+# - check if running admin perms??
+#
+# wezterm
+# fastfetch
+# starship
+# yazi
+# eza
+# fzf
+# bat
+# pwsh profile
+# scripts???
+#
+# glazewm and zebar
+# - open on startup
+#
+# hide taskbar?
+#
+# tbd...
+
+
